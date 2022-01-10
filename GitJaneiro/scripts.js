c1d868a87af9ede9d3bb58eace4034f2410b4648
Diógenes Ribeiro <diogenes.riboliveira@gmail.com>
2022-01-10 20:13:35 -0300

    Hardys

---
+++ GitJaneiro/scripts.js
@@ -0,0 +1,23 @@
+let oneModal = document.querySelector('.modal')
+let buttonSaibaMais = document.getElementById('saiba-mais')
+let buttonEsconder = document.getElementById('esconder')
+let oneContainer = document.querySelector('.container')
+
+
+buttonSaibaMais.addEventListener('click', () => {
+    oneModal.style.display = 'block'
+    buttonEsconder.style.display = 'block'
+})
+
+buttonEsconder.addEventListener('click', () => {
+    oneModal.style.display = 'none'
+    buttonEsconder.style.display = 'none'
+})
+
+window.addEventListener('scroll', function(e){
+    if(window.pageYOffset > 100){
+        oneContainer.style.background = 'rgba(0, 0, 0, .6)'
+    }else{
+        oneContainer.style.background = 'rgba(0, 0, 0, .4)'
+    }
+})
